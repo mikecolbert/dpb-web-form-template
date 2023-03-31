47c3b9273c2f9b100a829b4d9b906f9b6b07711c
mikecolbert <michael-colbert@uiowa.edu>
2023-03-31 09:14:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "alsknq3rAg$GernaeasSEF^woei4r098HRFYUKioq73498"
-app.config["MESSAGE_TIMEOUT"] = 2
+
 
 friends_dict = [
     {"name": "Test", "flavor": "swirl", "read": "yes", "activities": "reading"}
